3544fc0704dc0b42b0ed02d13e88bd7f745e0d22
PauloGarcesCarrasco <paulo.garces.carrasco@gmail.com>
2021-10-18 18:24:09 -0300

    first commit

---
+++ src/main/java/webscraping/webscrapingProyecto/App.java
@@ -0,0 +1,17 @@
+package webscraping.webscrapingProyecto;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+
+}
